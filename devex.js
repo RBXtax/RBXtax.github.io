cdb6af9083dfa7ef12eed1bf7fe2dfbbfe2a871c
RBXtax <121889358+RBXtax@users.noreply.github.com>
2024-10-20 02:57:41 +0100

    Update devex.js

--- devex.js
+++ devex.js
@@ -12,9 +12,10 @@ var left_url = [
 var right_url = [
     'https://gemsloot.com/?aff=RbxTax',
     'https://discord.gg/gz957GXktT',
+    'https://t.ly/rHrX6',
 ]
 var num_left_banners = 3
-var num_right_banners = 2
+var num_right_banners = 3
 
 // CHANGE THIS ONLY
 
