860f3c9c96b31b2a85b4aa17df37a7c4b109ae1a
RBXtax <121889358+RBXtax@users.noreply.github.com>
2024-10-20 03:07:10 +0100

    Update tax.js

--- tax.js
+++ tax.js
@@ -11,9 +11,10 @@ var left_url = [
 var right_url = [
     'https://t.ly/LsYHD',
     'https://discord.gg/kloonservices',
+    'https://discord.gg/dhgate',
 ]
 var num_left_banners = 2
-var num_right_banners = 2
+var num_right_banners = 3
 
 // CHANGE THIS ONLY
 
