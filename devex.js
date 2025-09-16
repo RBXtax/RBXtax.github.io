91674869111f89fb460ff4e3b34773072cb24e59
RBXtax <121889358+RBXtax@users.noreply.github.com>
2025-09-16 21:57:55 +0100

    Update devex.js

--- devex.js
+++ devex.js
@@ -8,7 +8,7 @@ var left_url = [
     'https://www.kloonboosts.com/',
 ]
 var right_url = [
-    'https://bloxdrop.com/',
+    'https://ishortn.ink/rLYX6sTb7',
 
 ]
 var num_left_banners = 1
@@ -117,5 +117,6 @@ window.onload = function() {
     var j = Math.floor(Math.random() * num_right_banners)+1;
     changeBanner(i, j)
 }
+
 
 
