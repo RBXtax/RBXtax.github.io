b0b203bfe7329b51902067bbcbf280cf7e9da5b3
RBXtax <121889358+RBXtax@users.noreply.github.com>
2025-12-11 23:03:10 +0000

    Update devex.js

--- devex.js
+++ devex.js
@@ -12,7 +12,7 @@ var left_url = [
 var right_url = [
     'https://rorush.com/welcome/?r=rbxtaxr1',
     'https://bloxdrop.com/',
-    'https://www.roblox.com/communities/55223267/kawaw#!/about',
+    'https://rbxstock.cc/',
 
 ]
 var num_left_banners = 3
@@ -121,6 +121,7 @@ window.onload = function() {
     var j = Math.floor(Math.random() * num_right_banners)+1;
     changeBanner(i, j)
 }
+
 
 
 
