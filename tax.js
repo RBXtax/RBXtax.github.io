9755a9c73aa3fa850186b85219b0b4d1f04fda07
RBXtax <121889358+RBXtax@users.noreply.github.com>
2026-01-18 20:02:20 +0000

    Update tax.js

--- tax.js
+++ tax.js
@@ -13,7 +13,7 @@ var left_url = [
 var right_url = [
     'https://rorush.com/welcome/?r=rbxtaxr1',
     'https://bloxdrop.com/',
-    'discord.gg/robloxlims',
+    'http://discord.gg/robloxlims',
 
 ]
 var num_left_banners = 3
@@ -168,6 +168,7 @@ window.onload = function() {
     changeBanner(i, j)
     loadbanners()
 }
+
 
 
 
