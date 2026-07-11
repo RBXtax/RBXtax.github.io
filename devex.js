3bc105b034b452ee24c367754d81dd0e43c47d57
RBXtax <121889358+RBXtax@users.noreply.github.com>
2026-07-11 20:03:56 +0100

    Update devex.js

--- devex.js
+++ devex.js
@@ -8,7 +8,7 @@ var left_url = [
     'https://rorush.com/welcome/?r=rbxtaxm2',
 ]
 var right_url = [
-    'discord.gg/xtsvaltus',
+    'https://discord.com/invite/xtsvaltus',
 
 ]
 var num_left_banners = 1
