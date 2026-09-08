1e5c0072174e94daf5174df6082ac285d5085340
RBXtax <121889358+RBXtax@users.noreply.github.com>
2026-09-08 18:11:27 +0100

    Update devex.js

--- devex.js
+++ devex.js
@@ -6,11 +6,11 @@
 
 var left_url = [
     'https://rorush.com/welcome/?r=rbxtaxm2',
-    'https://discord.gg/y2k',
+    'https://discord.gg/rmsT2HVQkG',
 ]
 var right_url = [
     'https://discord.com/invite/xtsvaltus',
-    'https://discord.gg/y2k',
+    'https://discord.gg/rmsT2HVQkG',
 
 ]
 var num_left_banners = 2
